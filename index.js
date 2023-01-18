e780265a3f5b00ed498e254febf666462bdd4a53
Dannybros <akbaromega89@gmail.com>
2023-01-18 13:50:29 +0700

    initial setup

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(process.env.MONGODB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+})
+mongoose.connection.on("connected", ()=>{
+    console.log("Mongoose is connected");
+})
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res)=>{
+    res.send("initial setup");
+})
+
+app.listen(port, ()=>console.log(`Server starting in port ${port}`));
