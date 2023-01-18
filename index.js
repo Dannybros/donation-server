ce745df944ad722cbca654825120df775758418f
Dannybros <akbaromega89@gmail.com>
2023-01-18 14:21:20 +0700

    fully load server

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import cases from './routes/Cases.js'
+import donation from './routes/Donation.js'
+import news from './routes/News.js'
+import signIn from './routes/SignIn.js'
+import stripe from './routes/Stripe.js'
+import paypal from './routes/Paypal.js'
 
 dotenv.config();
 
@@ -22,7 +27,12 @@ mongoose.connection.on("connected", ()=>{
 app.use(express.json());
 app.use(cors());
 
-app.use('/cases', cases)
+app.use('/donation', donation);
+app.use('/stripe', stripe);
+app.use('/news', news);
+app.use('/cases', cases);
+app.use('/signIn', signIn);
+app.use('/paypal', paypal);
 
 app.get('/', (req, res)=>{
     res.send("initial setup");
